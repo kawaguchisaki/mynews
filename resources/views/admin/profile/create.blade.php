4125b6da9cb765320c940e4917663fc39ae9def9
kawaguchi saki <amsn17.saki15@gmail.com>
2021-10-18 06:49:47 +0000

    mynews

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+        <title>create</title>
+    </head>
+    <body>
+        <h1>これは課題４のcreate.blade.phpです。</h1>
+    </body>
+</html>
